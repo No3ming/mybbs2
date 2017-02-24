5c64fe4e7ccec33cc07f6850e379c9b3f781980b
No3ming <540306318@qq.com>
2017-02-24 19:09:14 +0800

    asdasd

--- public/src/page/Editemd.vue
+++ public/src/page/Editemd.vue
@@ -1,22 +1,26 @@
 <template>
-    <div id="test-editormd"></div>
+    <div id="test-editormd">
+    </div>
 </template>
 
 <script>
   //import jquery from '../../static/libs/bower_components/editor.md/examples/js/jquery.min.js'
-  import raphael from 'raphael'
+  //import raphael from 'raphael'
   import editormd from '../../static/libs/bower_components/editor.md/editormd.js'
-  import  "../../static/libs/bower_components/editor.md/languages/en"
-  import  "../../static/libs/bower_components/editor.md/plugins/link-dialog/link-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/reference-link-dialog/reference-link-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/image-dialog/image-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/code-block-dialog/code-block-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/table-dialog/table-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/emoji-dialog/emoji-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/goto-line-dialog/goto-line-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/help-dialog/help-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/html-entities-dialog/html-entities-dialog"
-  import  "../../static/libs/bower_components/editor.md/plugins/preformatted-text-dialog/preformatted-text-dialog"
+  /*import  '../../static/libs/bower_components/editor.md/lib/codemirror/codemirror.min.css'
+  import  '../../static/libs/bower_components/editor.md/lib/codemirror/addon/fold/foldgutter.css'*/
+
+  /*require("../../static/libs/bower_components/editor.md/languages/en.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/link-dialog/link-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/reference-link-dialog/reference-link-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/image-dialog/image-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/code-block-dialog/code-block-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/table-dialog/table-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/emoji-dialog/emoji-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/goto-line-dialog/goto-line-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/help-dialog/help-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/html-entities-dialog/html-entities-dialog.js")(editormd)
+  require("../../static/libs/bower_components/editor.md/plugins/preformatted-text-dialog/preformatted-text-dialog.js")(editormd)*/
 
   export default {
     data () {
@@ -27,28 +31,33 @@
     mounted () {
       this.axios.get("/static/libs/bower_components/editor.md/examples/test.md")
         .then((rep) =>{
-          editormd("test-editormd", {
+          const testEditor = editormd("test-editormd", {
             width: "90%",
-            height: 640,
-            path : '/static/libs/bower_components/editor.md/lib/',
+            height: 740,
+            path : '/static/libs/editor.md/lib/',
+            theme : "dark",
+            previewTheme : "dark",
+            editorTheme : "pastel-on-dark",
             markdown : rep.data,
-            //toolbar  : false,             // 关闭工具栏
             codeFold : true,
+            //syncScrolling : false,
+            saveHTMLToTextarea : true,    // 保存 HTML 到 Textarea
             searchReplace : true,
-            saveHTMLToTextarea : true,      // 保存 HTML 到 Textarea
+            //watch : false,                // 关闭实时预览
             htmlDecode : "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启
+            //toolbar  : false,             //关闭工具栏
+            //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
             emoji : true,
             taskList : true,
-            tocm            : true,          // Using [TOCM]
-            tex : true,                      // 开启科学公式 TeX 语言支持，默认关闭
-            //previewCodeHighlight : false,  // 关闭预览窗口的代码高亮，默认开启
-            flowChart : true,                // 疑似 Sea.js与 Raphael.js 有冲突，必须先加载 Raphael.js ，Editor.md 才能在 Sea.js 下正常进行；
-            sequenceDiagram : true,          // 同上
-            //dialogLockScreen : false,      // 设置弹出层对话框不锁屏，全局通用，默认为 true
-            //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为 true
-            //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为 true
-            //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为 0.1
-            //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为 #fff
+            tocm            : true,         // Using [TOCM]
+            tex : true,                   // 开启科学公式TeX语言支持，默认关闭
+            flowChart : true,             // 开启流程图支持，默认关闭
+            sequenceDiagram : true,       // 开启时序/序列图支持，默认关闭,
+            //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
+            //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
+            //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
+            //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
+            //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
             imageUpload : true,
             imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
             imageUploadURL : "./php/upload.php",
